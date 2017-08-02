70f67db844211f690578a8d33740741c2e712ae7
David Tan <dt1041@nyu.edu>
2017-08-02 15:03:42 +0000

    Fix path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: "./lib/main.js",
   output: {
-    path:  __dirname + "./lib",
+    path:  __dirname + "/lib",
   	filename: "alien_raiders.js"
   },
   resolve: {
